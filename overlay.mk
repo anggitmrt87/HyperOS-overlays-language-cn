4839219654203409bf5e1c41f38a3be46ffe1d67
anggitmrt87 <jarbull87@gmail.com>
2026-07-08 06:16:24 +0700

    add appvault overlays

--- overlay.mk
+++ overlay.mk
@@ -2,6 +2,7 @@ PRODUCT_PACKAGES += \
     # product overlay
     framework-ext-res-multilang \
 	framework-res-multilang \
+	MIUIPersonalAssistantPhoneOS3-multilang \
 	miuisystem-multilang \
 	MiuiSystemUI-multilang \
 	miuix-multilang \
