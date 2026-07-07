2591393a4d8e68b17e6772c601d3e8fc1edfc5f1
anggitmrt87 <jarbull87@gmail.com>
2026-07-07 07:11:57 +0700

    add some overlays

--- overlay.mk
+++ overlay.mk
@@ -1,5 +1,8 @@
 PRODUCT_PACKAGES += \
     # product overlay
+    miuisystem-multilang \
+    framework-ext-res-multilang \
 	framework-res-multilang \
 	MiuiSystemUI-multilang \
+	miuix-multilang \
 	Settings-multilang
